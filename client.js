2b893915b379887887febc96dbfd20c6f462c5fa
Ricardo M. Vilchis <rmartinez@grupoabraxas.com>
2019-04-11 15:39:54 -0500

    Add support for graphql fragments

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ const debug = require("debug")("graphql-client")
 const gql = require("graphql-tag")
 const { createHttpLink } = require('apollo-link-http')
 const { onError } = require('apollo-link-error')
+const { IntrospectionFragmentMatcher } = require('apollo-cache-inmemory')
 
 const program = require("commander")
 
@@ -13,17 +14,59 @@ const errorLink = onError(({ networkError }) => {
     debug('networkError :', networkError)
   }
 })
-
-const run = (uri, token, query_string) => {
-  const cache = new InMemoryCache()
-  const headers = {Authorization: `Bearer ${token}`}
-  const link = errorLink.concat(createHttpLink({uri, fetch, headers}))
-  const client = new ApolloClient({link, cache})
-  const query = gql`${query_string}`
-  return client.query({query})
-    .then(JSON.stringify)
-    .catch(debug)
+const get_schema = (uri, token) => {
+  return fetch(`${uri}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      variables: {},
+      query: `
+      {
+        __schema {
+          types {
+            kind
+            name
+            possibleTypes {
+              name
+            }
+          }
+        }
+      }
+    `,
+    }),
+  })
+  .then(result => result.json())
+  .then(result => {
+    const filteredData = result.data.__schema.types.filter(
+      type => type.possibleTypes !== null,
+    );
+    result.data.__schema.types = filteredData;
+    return result.data
+  });
 }
+
+const run = (uri, token, query_string, variables) => {
+  return get_schema(uri, token).then(data => {
+    const fragmentMatcher = new IntrospectionFragmentMatcher({
+      introspectionQueryResultData: data
+    });
+    const cache = new InMemoryCache({fragmentMatcher})
+    const headers = {Authorization: `Bearer ${token}`}
+    const link = errorLink.concat(createHttpLink({uri, fetch, headers}))
+    const client = new ApolloClient({link, cache})
+    return client
+      .query({
+        query: gql`${query_string}`,
+        variables: JSON.parse(variables)
+      })
+      .then(JSON.stringify)
+      .catch(debug)
+  }).catch(debug)
+
+ }
 
 const mutate = (uri, token, query_string, variables) => {
   const cache = new InMemoryCache()
@@ -43,13 +86,15 @@ program
   .version('0.1.0')
 
 program
-  .command('query <host> <token> <query>')
+  .command('query <host> <token> <query> [data]')
   .alias('q')
-  .action((host, token, query) => {
+  .action((host, token, query, data) => {
     debug(`Host: ${host}`)
     debug(`Token: ${token}`)
     debug(`Query: ${query}`)
-    run(host, token, query)
+    debug(`Data: ${data}`)
+    data = data || "{}"
+    run(host, token, query, data)
       .then(console.log)
       .catch(debug)
   })
